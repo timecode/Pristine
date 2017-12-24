54abb6d488e998c89b17f26eccb7636c1622c26f
Rob Playford <rob.playford@gmail.com>
2017-12-24 15:04:18 +0000

    Uncomment dev disable of Prezto install

--- setup.sh
+++ setup.sh
@@ -7,20 +7,20 @@ set -e
 # apt install zsh
 # apt install git
 
-# # mac OS
-# brew install git
-#
-# # Install prezto
-# rm -rf "${ZDOTDIR:-$HOME}/.zprezto"
-# git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-#
-# # Configure prezto
-# setopt EXTENDED_GLOB
-# for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-#   rm "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-#   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-#   echo "linking ${rcfile:t}"
-# done
+# mac OS
+brew install git
+
+# Install prezto
+rm -rf "${ZDOTDIR:-$HOME}/.zprezto"
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+# Configure prezto
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  rm "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+  echo "linking ${rcfile:t}"
+done
 
 # Install Powerline fonts
 # clone
@@ -55,7 +55,6 @@ echo "OK, now exit this shell and create a new one, and you should be all set."
 ## PERFORMING UPDATES
 # Run this command ...
 # $ zprezto-update
-# or
-# To pull the latest changes and update submodules manually:
+# or, pull the latest changes and update submodules manually with:
 # $ cd $ZPREZTODIR
-# git pull && git submodule update --init --recursive
+# $ git pull && git submodule update --init --recursive
