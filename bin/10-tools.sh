d013b64fea72bc3ad3227a20208201db798bacdd
Rob Playford <rob.playford@gmail.com>
2018-03-09 12:29:28 +0000

    install node before brew

--- bin/10-tools.sh
+++ bin/10-tools.sh
@@ -6,60 +6,79 @@
 #####################################
 # mac OS
 #####################################
-# /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-# /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall)"
 
 echo ""
 if type "brew" > /dev/null; then
-  brew upgrade
-  echo "Installing tool dependencies ..."
-  brew install    \
-    lesspipe      \
-    git           \
-    awscli        \
-    gpg-agent     \
-    python        \
-    python2       \
-    rbenv         \
-    yarn          \
-    go            \
-    tree          \
-    jq            \
-    ncdu          \
-    wget          \
-    tmux          \
-    tor           \
-    stow
-
-  # adns		  gradle		 packer
-  # libffi    libusb-compat
-  # gmp		    jfrog-cli-go		md5sha1sum
-  # cdrtools	gnupg		        p11-kit	    watch
-  # coreutils	gnupg2	   kops	    nettle	    p7zip	 webkit2png
-  # dirmngr		gnutls	   nmap	    	    redis
-  # libtasn1 	npth       ec2-ami-tools  libusb
-
+  echo "Homebrew already installed :-)"
 else
-  echo "Please install Homebrew first ..."
+  # /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  # /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall)"
+  echo "Please install Homebrew first..."
   echo "... find it here: https://brew.sh/"
   echo ""
   exit 1
 fi
 
 # install latest nvm and node
-if [ -d $HOME/.nvm ]; then
-  export NVM_DIR="$HOME/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-  nvm install-latest-npm
-  nvm use node
-else
-  wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+echo "Installing nvm, node, npm..."
+
+if [ -f /usr/local/bin/npm ]; then
+  echo "Removing non-nvm installed npm..."
+  rm /usr/local/bin/npm
 fi
 
-nvm install node
+if [ -d $HOME/.nvm ]; then
+  echo "nvm already installed..."
+else
+  # https://github.com/creationix/nvm
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+  # wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+fi
 
-# install node modules
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
+echo ""
+echo "Installing latest node..."
+nvm install node
+nvm install-latest-npm
+npm config delete prefix
+nvm use node
+
+echo ""
+echo "Installing node modules..."
 npm install spoof -g
+
+echo ""
+echo "Checking Homebrew..."
+brew upgrade
+brew doctor
+echo "Installing tool dependencies..."
+brew install          \
+  lesspipe            \
+  git                 \
+  awscli              \
+  gpg-agent           \
+  python              \
+  python2             \
+  rbenv               \
+  yarn --without-node \
+  go                  \
+  tree                \
+  jq                  \
+  ncdu                \
+  wget                \
+  tmux                \
+  tor                 \
+  stow
+
+# adns		  gradle		 packer
+# libffi    libusb-compat
+# gmp		    jfrog-cli-go		md5sha1sum
+# cdrtools	gnupg		        p11-kit	    watch
+# coreutils	gnupg2	   kops	    nettle	    p7zip	 webkit2png
+# dirmngr		gnutls	   nmap	    	    redis
+# libtasn1 	npth       ec2-ami-tools  libusb
 
 #####################################
 # Linux
