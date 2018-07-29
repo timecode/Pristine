33f4fa122b4978a240a915600536ab95c413f6bf
Rob Playford <rob.playford@gmail.com>
2018-07-29 09:54:50 +0100

    use latest nvm installer

--- bin/10-tools.sh
+++ bin/10-tools.sh
@@ -21,7 +21,8 @@ fi
 
 ######################################
 # nvm and node
-echo "Installing nvm, node, npm..."
+echo "Checking nvm, node, npm status..."
+echo ""
 
 if [ -f /usr/local/bin/npm ]; then
   echo "Removing non-nvm installed npm..."
@@ -31,16 +32,17 @@ fi
 if [ -d $HOME/.nvm ]; then
   echo "nvm already installed..."
 else
+  echo "Installing nvm..."
   # https://github.com/creationix/nvm
-  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
-  # wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+  # wget -qO- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 fi
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
 echo ""
-echo "Installing latest node..."
+echo "Ensuring latest node..."
 nvm install node
 nvm install-latest-npm
 npm config delete prefix
