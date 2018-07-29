8738ced79da42964deb8ebda75e217acbd920484
Rob Playford <rob.playford@gmail.com>
2018-07-29 09:30:39 +0100

    update repo first

---
+++ bin/00-pull-latest.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+################################################################################
+# Update this repo
+
+echo "Attempting to update this repo first..."
+cd $0/../../
+git pull
