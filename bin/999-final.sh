8e5b252ffeda76a3641441e9625c20dfd19e1aa4
Rob Playford <rob.playford@gmail.com>
2018-10-18 07:49:49 +0100

    refactor final instructions to their own step

---
+++ bin/999-final.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+echo ""
+echo "Done! Now exit this shell and create a new one to use the new setup..."
+echo ""
+echo "or run:"
+echo 'exec "$SHELL"'
+echo ""
--- setup.sh
+++ setup.sh
@@ -7,11 +7,3 @@ FILES=($(ls ${scriptDirectory}/bin | sort -n))
 for file in ${FILES[@]} ; do
   ${scriptDirectory}/bin/${file}
 done
-
-################################################################################
-echo ""
-echo "Done! Now exit this shell and create a new one to use the new setup..."
-echo ""
-echo "Or run:"
-echo 'exec "$SHELL"'
-echo ""
