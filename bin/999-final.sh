a9278d1af501a265021d749d2acd0ba4caebadbf
Rob Playford <rob.playford@gmail.com>
2018-10-25 16:20:20 +0100

    attempt auto shell roll

--- bin/999-final.sh
+++ bin/999-final.sh
@@ -4,5 +4,8 @@ echo ""
 echo "Done! Now exit this shell and create a new one to use the new setup..."
 echo ""
 echo "or run:"
-echo '$ exec "$SHELL"'
+echo 'exec "$SHELL"'
 echo ""
+echo ""
+echo "Will attempt to automatically load a new shell now anyway, so this session can continue..."
+exec "$SHELL"
