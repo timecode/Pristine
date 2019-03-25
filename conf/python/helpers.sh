d6988fbf38df3f78dd23556be49630c7e70d7c08
Rob Playford <rob.playford@gmail.com>
2019-03-25 12:38:37 +0000

    fix system pip installs

--- conf/python/helpers.sh
+++ conf/python/helpers.sh
@@ -1,11 +1,11 @@
 function pip2_install() {
   local arr=("$@")
   echo "Installing global pip packages"
-  PIP_REQUIRE_VIRTUALENV="" pip2 install -U "${arr[@]}"
+  PIP_REQUIRE_VIRTUALENV=false pip2 install -U "${arr[@]}"
 }
 
 function pip3_install() {
   local arr=("$@")
   echo "Installing global pip packages"
-  PIP_REQUIRE_VIRTUALENV="" pip3 install -U "${arr[@]}"
+  PIP_REQUIRE_VIRTUALENV=false pip3 install -U "${arr[@]}"
 }
