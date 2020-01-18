4fb3edba9cc52f9e0a787dff280a5fb41920cde9
Rob Playford <rob.playford@gmail.com>
2020-01-18 15:51:21 +0000

    stop dotfile errors crashing setup

--- bin/40-dotfiles.sh
+++ bin/40-dotfiles.sh
@@ -5,55 +5,78 @@ scriptDirectory=$(exec 2>/dev/null; cd -- $(dirname "$0"); /usr/bin/pwd || /bin/
 ################################################################################
 # Install dotfiles
 
-echo ""
-echo "Setting up dotfiles ..."
-if ! type "stow" > /dev/null; then
-  echo "... installing stow"
-  brew install stow
-fi
+function setup_dotfiles() {
+  echo ""
+  echo "Setting up dotfiles ..."
+  if ! type "stow" > /dev/null; then
+    echo "... installing stow"
+    brew install stow
+  fi
+}
 
-#####################################
-echo "... bootstrapping stow"
+function bootstrap_stow() {
+  echo "... bootstrapping stow"
 
-cat <<EOF > $HOME/.stowrc
+  cat <<EOF > $HOME/.stowrc
 --dir=${scriptDirectory}/../dotfiles
 --target=$HOME
 --ignore=.DS_Store
 EOF
+}
 
-#####################################
-if [[ -f "${ZDOTDIR:-$HOME}/.zshrc" ]]; then
-  echo "... backing-up existing .zshrc ..."
-  cp ${ZDOTDIR:-$HOME}/.zshrc ${ZDOTDIR:-$HOME}/.zshrc_bak
-  rm -f ${ZDOTDIR:-$HOME}/.zshrc
-fi
+function backup_zshrc() {
+  if [[ -f "${ZDOTDIR:-$HOME}/.zshrc" ]]; then
+    echo "... backing-up existing .zshrc ..."
+    cp ${ZDOTDIR:-$HOME}/.zshrc ${ZDOTDIR:-$HOME}/.zshrc_bak
+    rm -f ${ZDOTDIR:-$HOME}/.zshrc
+  fi
+}
 
-#####################################
-# Add directory/names of apps below to have
-# their dotfiles installed by stow during setup
-stow -v --stow  \
-  stow          \
-  bash          \
-  pip           \
-  zsh           \
-  vim           \
-  qlmarkdown    \
-  tmux          \
-  docker
+function stow_dotfiles() {
+  # Add directory/names of apps below to have
+  # their dotfiles installed by stow during setup
+  echo "... running stow (general) ..."
+  stow -v --stow  \
+    stow          \
+    bash          \
+    pip           \
+    zsh           \
+    vim           \
+    qlmarkdown    \
+    tmux          \
+    docker
+}
 
-#####################################
-# special settings for iterm2
-stow -v --stow  \
-  iterm2
-# specify preferences directory (now under dotfile control)
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.iterm2_profile"
-# use custom preferences
-defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+function setup_iterm2() {
+  echo "... running stow (iterm2) ..."
+  # special settings for iterm2
+  rm -rf ~/.iterm2_profile/com.googlecode.iterm2.plist
+  stow -v --stow  \
+    iterm2 && \
+  # specify preferences directory (now under dotfile control)
+  defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.iterm2_profile" && \
+  # use custom preferences
+  defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+}
 
-#####################################
-# special settings for vscode as config is not dotfile compatible
-# open a vanilla install of vscode
-# install settings-sync
-# https://marketplace.visualstudio.com/items?itemName=Shan.code-settings-sync
-# `shift + alt + D`
-# to download last sync'd settings
+function setup_vscode() {
+  # special settings for vscode as config is not dotfile compatible
+  # open a vanilla install of vscode
+  # install settings-sync
+  # https://marketplace.visualstudio.com/items?itemName=Shan.code-settings-sync
+  # `shift + alt + D`
+  # to download last sync'd settings
+}
+
+{ # try
+  setup_dotfiles && \
+  bootstrap_stow && \
+  setup_iterm2 && \
+  backup_zshrc && \
+  stow_dotfiles && \
+  echo "... dotfiles complete"
+} || { # catch
+  echo ""
+  echo "ERROR with dotfiles... see above"
+}
+
