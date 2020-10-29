c17a0feb4e3d044c07e4f67b56f7b6f8c9f70820
Rob Playford <rob.playford@gmail.com>
2020-10-29 08:05:35 +0000

    back to python3.9 now it appears to have been fixed

--- bin/10-tools.sh
+++ bin/10-tools.sh
@@ -119,8 +119,8 @@ brew_install_casks "${language_casks[@]}"
 # brew cask upgrade "${language_casks[@]}"
 
 declare -a language_bottles=(
-  python@3.8
-  # python@3.9
+  # python@3.8
+  python@3.9
   rbenv
   go
   yarn
@@ -170,12 +170,12 @@ brew cleanup
 # python installs
 
 echo
-echo "Forcing python3.8 to be default..."
-ln -fs /usr/local/bin/python3.8 /usr/local/bin/python
-ln -fs /usr/local/bin/pip3 /usr/local/bin/pip
-# echo "Forcing python3.9 to be default..."
-# ln -fs /usr/local/opt/python@3.9/bin/python3 /usr/local/bin/python
-# ln -fs /usr/local/opt/python@3.9/bin/pip3 /usr/local/bin/pip
+# echo "Forcing python3.8 to be default..."
+# ln -fs /usr/local/bin/python3.8 /usr/local/bin/python
+# ln -fs /usr/local/bin/pip3 /usr/local/bin/pip
+echo "Forcing python3.9 to be default..."
+ln -fs /usr/local/opt/python@3.9/bin/python3 /usr/local/bin/python
+ln -fs /usr/local/opt/python@3.9/bin/pip3 /usr/local/bin/pip
 
 echo
 echo "Setting up python environment..."
