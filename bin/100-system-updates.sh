c843032ecc4d0ea47b1b413a58bf476e74b716f9
Rob Playford <rob.playford@gmail.com>
2020-11-13 16:01:35 +0000

    add system update check

---
+++ bin/100-system-updates.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+################################################################################
+# Update the Mac OS and dependencies
+
+echo
+echo "Checking for system updates ..."
+
+softwareupdate --install --all
